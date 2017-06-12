011f11b6ba88cdf1bcde54a063b4ec2238e41c85
Eric Kawaguchi <erickawaguchi@ucla.edu>
2017-06-12 14:24:32 -0700

    added test for coxbar solutions

---
+++ tests/testthat/test-CoxbarOldNewSol.R
@@ -0,0 +1,24 @@
+library("testthat")
+library("survival")
+library("BrokenAdaptiveRidge")
+source("extras/coxbar.R")
+source("extras/bar_fit.R")
+
+test_that("CoxBAR old and new code same sol", {
+test <- read.table(header=T, sep = ",", text = "
+start, length, event, x1, x2
+0, 4, 1,0,0
+0, 3.5,1,2,0
+0, 3, 0,0,1
+0, 2.5,1,0,1
+0, 2, 1,1,1
+0, 1.5,0,1,0
+0, 1, 1,1,0
+")
+
+fit.old <- coxbar(test$length, test$event, cbind(test$x1, test$x2), lambda = 0.1, xi = 0.1, old = TRUE)
+fit.new <- coxbar(test$length, test$event, cbind(test$x1, test$x2), lambda = 0.1, xi = 0.1, old = FALSE)
+
+expect_equal(fit.old$beta,  fit.new$beta)
+}
+)
